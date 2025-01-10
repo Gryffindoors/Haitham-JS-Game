95b93b198a429163096cc8dff876aa24bfdb4db5
Gryffindoors <goldengryffin010@gmail.com>
2025-01-10 23:34:45 +0200

    one more game1

--- Js/xo.js
+++ Js/xo.js
@@ -1,4 +1,4 @@
-// Updated JavaScript for XO Game with Winning Line Feature
+// Updated JavaScript for XO Game with Winning Line Fix
 
 const iconX = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w-50 text-success" fill="currentColor">
     <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8-9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
@@ -73,6 +73,7 @@ function checkWin(playerMoves) {
 }
 
 $("#startBtn").click(function () {
+    $(".winning-line").remove(); // Remove any existing winning line
     $("#board").removeClass("text-bg-danger text-bg-secondary").addClass("text-bg-warning").fadeIn(1000);
     $("#infoTitle").text("Game Started");
     $("#info").text("Player 1's Turn");
@@ -130,5 +131,5 @@ $("#resetBtn").click(function () {
     player2 = [];
     clickCount = 0;
     $(".play-area").html("").removeClass("winner").off("click").off("mouseover");
-    document.querySelector(".winning-line")?.remove(); // Remove the winning line
+    $(".winning-line").remove(); // Remove the winning line
 });
